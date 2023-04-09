2fa4f388a0394801261149194926eb227950b987
andrew-kam <andrewkamvik@gmail.com>
2023-04-09 11:54:51 +0300

    add generateRegion

--- src/main/kotlin/GeneratePerson.kt
+++ src/main/kotlin/GeneratePerson.kt
@@ -5,6 +5,8 @@ import java.util.Locale
 
 class GeneratePerson {
 
+    private val fake = Faker(Locale("ru_RU"))
+
     fun generatePerson(): Person {
 
         val fullNameAndGender = generateFullNameAndGender()
@@ -14,14 +16,18 @@ class GeneratePerson {
         val middleName = fullNameAndGender[2]
 
         val age = 30
+
         val gender = fullNameAndGender[3]
+
 //    val birthDate = fake.date().birthday(18, 88)
         val birthDate = "01-01-1970"
+
         val birthPlace = fake.address().city()
         val postalCode = fake.address().zipCode()
         val country = nameCountry
-        val region = fake.address().state()
-        val city = fake.address().cityName()
+        val region = generateRegion()
+
+        val city = fake.address().city()
         val street = fake.address().streetName()
         val houseNumber = Random.nextInt(1, 200)
         val apartmentNumber = Random.nextInt(1, 500)
@@ -52,8 +58,7 @@ class GeneratePerson {
             fullNameAndGender.add(lastNamesMan.random())
             fullNameAndGender.add(middleNamesMan.random())
             fullNameAndGender.add(male)
-        }
-        else {
+        } else {
             fullNameAndGender.add(firstNamesWoman.random())
             fullNameAndGender.add(lastNamesWoman.random())
             fullNameAndGender.add(middleNamesWoman.random())
@@ -62,7 +67,17 @@ class GeneratePerson {
         return fullNameAndGender
     }
 
-    private val fake = Faker(Locale("ru_RU"))
+    private fun generateRegion(): String {
+        var count = 0
+        var fullRegion = listOf<String>()
+        while (fullRegion.size != 2 || fullRegion[1] != "область") {
+            fullRegion = fake.address().state().split(" ")
+        }
+        return fullRegion[0]
+    }
+
+
+
 
 
 

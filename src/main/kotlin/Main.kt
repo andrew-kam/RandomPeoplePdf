9c3b46b014081eb0c81e120604f5eb64f01920cc
andrew-kam <andrewkamvik@gmail.com>
2023-04-09 10:23:28 +0300

    add fun input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,40 @@
 fun main() {
 
-    println("Введите количество людей ($minCountPeoples-$maxCountPeoples):")
-    val countPeoples = readlnOrNull()?.toIntOrNull() ?: 0
-    if (countPeoples < minCountPeoples || countPeoples > maxCountPeoples) {
-        println("Некорректное количество людей!")
+    val countPeople = inputCountPeople()
+    if (countPeople == 0) {
+        println("Введены некорректные данные, работа программы завершена!")
         return
+    }
+    println(countPeople)
+
+    val filePath = inputFilePath()
+    if (filePath.isEmpty()) {
+        println("Путь к файлу не может быть пустым, работа программы завершена!")
+    }
+    println(filePath)
+
+
+    val people = mutableListOf<Person>()
+    val personGenerator = GeneratePerson()
+    repeat(countPeople) {
+        people.add(personGenerator.generatePerson())
     }
 
-    println("Введите путь для сохранения файла\n(например D:\\TempPdf\\people.pdf):")
-    val filePath = readlnOrNull() ?: ""
-    if (filePath.isEmpty()) {
-        println("Путь не может быть пустым!")
-        return
+
+
+//    try {
+//        createPdf(filePath, people)
+//        println("Файл создан. Путь: $filePath")
+//    } catch (e: Exception) {
+//        println("Ошибка при создании файла.")
+//        e.printStackTrace()
+//        return
+//    }
+
+    people.forEach { human ->
+        println(human)
     }
+
 
 }
 

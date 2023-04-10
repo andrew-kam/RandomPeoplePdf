9777bc8b8876ceeeef1dbe4759cef74ff10f2af6
andrew-kam <andrewkamvik@gmail.com>
2023-04-10 14:34:34 +0300

    refactor

--- src/main/kotlin/GeneratePerson.kt
+++ src/main/kotlin/GeneratePerson.kt
@@ -12,43 +12,30 @@ class GeneratePerson {
     private val fake = Faker(Locale("ru_RU"))
 
     fun generatePerson(choiceGender: Boolean): Person {
-        val firstName = if (choiceGender) firstNamesMan.random() else firstNamesWoman.random()
-        val lastName = if (choiceGender) lastNamesMan.random() else lastNamesWoman.random()
-        val middleName = if (choiceGender) middleNamesMan.random() else middleNamesWoman.random()
-        val gender = if (choiceGender) male else female
+
         val (birthDate, age) = createBirthDateAndAge()
-        val birthPlace = fake.address().city()
-        val postalCode = Random.nextInt(minZipCode, maxZipCode).toString()
-        val country = nameCountry
-        val region = regionNames.random()
-        val city = fake.address().city()
-        val street = streetNames.random()
-        val houseNumber = Random.nextInt(minHouseNumber, maxHouseNumber).toString()
-        val apartmentNumber = Random.nextInt(minApartmentNumber, maxApartmentNumber).toString()
 
         return Person(
-            firstName,
-            lastName,
-            middleName,
-            age,
-            gender,
-            birthDate,
-            birthPlace,
-            postalCode,
-            country,
-            region,
-            city,
-            street,
-            houseNumber,
-            apartmentNumber
+            firstName = if (choiceGender) firstNamesMan.random() else firstNamesWoman.random(),
+            lastName = if (choiceGender) lastNamesMan.random() else lastNamesWoman.random(),
+            middleName = if (choiceGender) middleNamesMan.random() else middleNamesWoman.random(),
+            age = age,
+            gender = if (choiceGender) male else female,
+            birthDate = birthDate,
+            birthPlace = fake.address().city(),
+            postalCode = Random.nextInt(minZipCode, maxZipCode).toString(),
+            country = nameCountry,
+            region = regionNames.random(),
+            city = fake.address().city(),
+            street = streetNames.random(),
+            houseNumber = Random.nextInt(minHouseNumber, maxHouseNumber).toString(),
+            apartmentNumber = Random.nextInt(minApartmentNumber, maxApartmentNumber).toString()
         )
     }
-
 
     private fun createBirthDateAndAge(): Pair<String, String> {
 
         val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-
         val currentDate = LocalDate.now()
         val maxPeriod = Period.ofYears(maxAge)
         val minPeriod = Period.ofYears(minAge)
@@ -57,6 +44,7 @@ class GeneratePerson {
         val randomDate = startDate.plusDays(Random.nextLong(startDate.until(endDate, ChronoUnit.DAYS)))
         val randomDateString = randomDate.format(formatter)
         val age = Period.between(randomDate, currentDate).years.toString()
+
         return Pair(randomDateString, age)
     }
 }
